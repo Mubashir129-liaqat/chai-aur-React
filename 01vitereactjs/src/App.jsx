8795a4c507d9d5832591f411e89226d1b5dcfb67
Mubashir Liaqat <mubashirliaqat160@gmail.com>
2024-01-28 01:26:22 +0500

    interview questions

--- 01vitereactjs/src/App.jsx
+++ 01vitereactjs/src/App.jsx
@@ -10,6 +10,18 @@ function App() {
         onClick={() => {
           if (count < 20) {
             setCount(count + 1);
+            setCount(count + 1);
+            setCount(count + 1);
+            setCount(count + 1);
+            // will update the count by one not for four times
+            console.log(count);
+            //! so i founded the new way to do this...
+            setCount((prevCount) => prevCount + 1);
+            setCount((prevCount) => prevCount + 1);
+            setCount((prevCount) => prevCount + 1);
+            setCount((prevCount) => prevCount + 1);
+            //* Getting the Previous counter and updating it by my view...
+            console.log(count);
           } else {
             console.log("counter must be less than 20");
           }
